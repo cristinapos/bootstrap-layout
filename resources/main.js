6db1b0025188cf9e8395ee873ce041c95c57140a
s135988js028 <cristina.bairac@yahoo.ro>
2022-07-01 11:26:02 +0300

    Finishing the desktop layout

---
+++ resources/main.js
@@ -0,0 +1,17 @@
+const cursor = document.querySelector('.ball-one');
+
+const col = document.querySelectorAll('.col-3');
+
+col.forEach((el) => el.addEventListener('mousemove', function(e) {
+    cursor.style.display = "flex";
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+}))
+
+col.forEach((el) => el.addEventListener('mouseleave', function(e) {
+    cursor.style.top = 0 + "px";
+    cursor.style.left = 0 + "px";
+    cursor.style.display = "none";
+}))
+
+
